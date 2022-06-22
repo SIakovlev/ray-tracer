4728728952172ef75d40a13c297493ada3ff60d0
Sergey Iakovlev <siakovlev@student.unimelb.edu.au>
2022-06-22 23:49:14 +1000

    SI: add identity matrix, add more mul implementations, add submatrix/minor/det/cofactor functionality

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,22 +1,100 @@
 use std::ops::{Index, Mul};
+use num::Integer;
+
+use crate::tuple::Tuple;
 
 
 #[derive(Debug, PartialEq, PartialOrd)]
-struct Matrix<const M: usize, const N: usize> {
-    data: [[f32; N]; M]
+pub struct Matrix<const M: usize, const N: usize> {
+    pub data: [[f32; N]; M]
 }
 
 impl<const M: usize, const N: usize> Matrix<M, N> {
-    fn new(data: [[f32; N]; M]) -> Self {
+    pub fn new(data: [[f32; N]; M]) -> Self {
         Matrix { data: data }
     }
 
-    fn transpose(&self) -> Matrix<N, M> {
+    // construct identity matrix
+    pub fn identity() -> Self {
+        let mut tmp: [[f32; N]; M] = [[0.0; N]; M];
+        for row_idx in 0..M {
+            for col_idx in 0..N {
+                if row_idx == col_idx {
+                    tmp[row_idx][col_idx] = 1.0;
+                }
+            }
+        }
+        Matrix { data: tmp }
+    }
+
+    pub fn transpose(&self) -> Matrix<N, M> {
         let mut tmp: [[f32; M]; N] = [[0.0; M]; N];
         for (row_idx, row) in self.data.iter().enumerate() {
             for (col_idx, elem) in row.iter().enumerate() {
                 tmp[col_idx][row_idx] = *elem;
             }
+        }
+
+        Matrix { data: tmp }
+    }
+
+    pub fn minor(&self, row_idx: usize, col_idx: usize) -> f32 
+    where
+        [(); N - 1]:,
+        [(); M - 1]:,
+    {
+        // for now
+        assert!(N == 3 && M == 3);
+
+        let submatrix = self.submatrix(row_idx, col_idx);
+        submatrix.det()
+    }
+
+    pub fn cofactor(&self, row_idx: usize, col_idx: usize) -> f32 
+    where
+        [(); N - 1]:,
+        [(); M - 1]:,
+    {
+        // for now
+        assert!(N == 3 && M == 3);
+
+        let minor_value = self.minor(row_idx, col_idx);
+        if (row_idx + col_idx) % 2 == 0 {
+            minor_value
+        } else {
+            -minor_value
+        }
+    }
+
+    pub fn det(&self) -> f32 {
+
+        // for now
+        assert!(N == 2 && M == 2);
+
+        return self[(0, 0)] * self[(1, 1)] - self[(0, 1)] * self[(1, 0)]
+    }
+    
+    pub fn submatrix(&self, row_idx_skip: usize, col_idx_skip: usize) -> Matrix<{ M-1 }, { N-1 }> {
+
+        assert!(N > 1 && M > 1);
+
+        let mut tmp: [[f32; N-1]; M-1] = [[0.0; N-1]; M-1];
+    
+        let mut row_idx: usize = 0;
+        let mut col_idx: usize = 0;
+        for m in 0..M {
+            if m == row_idx_skip {
+                continue;
+            }
+            for c in 0..N {
+                if c == col_idx_skip {
+                    continue;
+                }
+                tmp[row_idx][col_idx] = self[(m, c)];
+                col_idx += 1
+            }
+            row_idx += 1;
+            col_idx = 0;
         }
 
         Matrix { data: tmp }
@@ -31,6 +109,7 @@ impl<const M: usize, const N: usize> Index<(usize, usize)> for Matrix<M, N> {
     }
 }
 
+// Implement multiplication by another MxN matrix
 impl<const M: usize, const N: usize> Mul<Matrix<N, M>> for Matrix<M, N> {
     type Output = Matrix<N, N>;
 
@@ -44,6 +123,21 @@ impl<const M: usize, const N: usize> Mul<Matrix<N, M>> for Matrix<M, N> {
             }
         }
         Matrix { data: tmp }
+    }
+}
+
+// Implement multiplication by tuple of 4 elements
+impl<const M: usize> Mul<Tuple> for Matrix<M, 4> {
+    type Output = Tuple;
+
+    fn mul(self, rhs: Tuple) -> Self::Output {
+        let mut tmp: [f32; 4] = [0.0; 4];
+        for (row_idx, row) in self.data.iter().enumerate() {
+            for (r, c) in row.iter().zip(rhs.into_iter()) {
+                tmp[row_idx] += r * c;
+            }
+        }
+        Tuple::from_array(tmp)
     }
 }
 
@@ -164,6 +258,119 @@ mod tests {
         );
 
         assert_eq!(&(m1 * m2), &m3);
+    }
+
+    #[test]
+    fn multiply_by_tuple() {
+        use crate::tuple::Tuple;
+
+        let m1 = Matrix::<4, 4>::new(
+            [[1.0, 2.0, 3.0, 4.0], 
+            [2.0, 4.0, 4.0, 2.0],
+            [8.0, 6.0, 4.0, 1.0],
+            [0.0, 0.0, 0.0, 1.0]]
+        );
+
+        let t1 = Tuple::new(1.0, 2.0, 3.0, 1.0);
+
+        let t = Tuple::new(18.0, 24.0, 33.0, 1.0);
+
+        assert_eq!(&(m1 * t1), &t);
+    }
+
+    #[test]
+    fn multiply_by_identity() {
+        use crate::tuple::Tuple;
+
+        let m1 = Matrix::<4, 4>::new(
+            [[1.0, 2.0, 3.0, 4.0], 
+            [2.0, 4.0, 4.0, 2.0],
+            [8.0, 6.0, 4.0, 1.0],
+            [0.0, 0.0, 0.0, 1.0]]
+        );
+        
+        let m_id = Matrix::<4, 4>::identity();
+
+        let m = Matrix::<4, 4>::new(
+            [[1.0, 2.0, 3.0, 4.0], 
+            [2.0, 4.0, 4.0, 2.0],
+            [8.0, 6.0, 4.0, 1.0],
+            [0.0, 0.0, 0.0, 1.0]]
+        );
+
+        assert_eq!(&(m1 * m_id), &m);
+
+        let m_id = Matrix::<4, 4>::identity();
+
+        let t1 = Tuple::new(1.0, 2.0, 3.0, 1.0);
+
+        let t = Tuple::new(1.0, 2.0, 3.0, 1.0);
+
+        assert_eq!(&(m_id * t1), &t);
+    }
+
+    #[test]
+    fn submatrix() {
+
+        let m1 = Matrix::<3, 3>::new(
+            [[1.0, 5.0, 0.0], 
+            [-3.0, 2.0, 7.0],
+            [0.0, 6.0, -3.0]]
+        );
+
+        let m2 = m1.submatrix(0, 2);
+
+        let m = Matrix::<2, 2>::new(
+            [[-3.0, 2.0],
+            [0.0, 6.0]]
+        );
+
+        assert_eq!(&m2, &m);
+
+        let m1 = Matrix::<4, 4>::new(
+            [[-6.0, 1.0, 1.0, 6.0], 
+            [-8.0, 5.0, 8.0, 6.0],
+            [-1.0, 0.0, 8.0, 2.0],
+            [-7.0, 1.0, -1.0, 1.0]]
+        );
+
+        let m2 = m1.submatrix(2, 1);
+
+        let m = Matrix::<3, 3>::new(
+            [[-6.0, 1.0, 6.0],
+            [-8.0, 8.0, 6.0],
+            [-7.0, -1.0, 1.0]]
+        );
+
+        assert_eq!(&m2, &m);
+
+    }
+
+    #[test]
+    fn minor() {
+
+        let m1 = Matrix::<3, 3>::new(
+            [[3.0, 5.0, 0.0], 
+            [2.0, -1.0, -7.0],
+            [6.0, -1.0, 5.0]]
+        );
+
+        assert_eq!(m1.minor(0, 0), -12.0);
+        assert_eq!(m1.minor(1, 0), 25.0);
+    }
+
+    #[test]
+    fn cofactor() {
+
+        let m1 = Matrix::<3, 3>::new(
+            [[3.0, 5.0, 0.0], 
+            [2.0, -1.0, -7.0],
+            [6.0, -1.0, 5.0]]
+        );
+
+        assert_eq!(m1.minor(0, 0), -12.0);
+        assert_eq!(m1.cofactor(0, 0), -12.0);
+        assert_eq!(m1.cofactor(1, 0), -25.0);
     }
 
 }
