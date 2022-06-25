766e3ddd8a23dca4ec8cb769013acb60c727e9ab
Sergey Iakovlev <siakovlev@student.unimelb.edu.au>
2022-06-26 00:09:40 +1000

    SI: add sphere shadow example

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ mod intersection;
 
 use std::cmp;
 
-fn main() {
+fn projectile_example() {
     let start = point::Point::new(0.0, 1.0, 0.0);
     let velocity = vector::Vector::new(1.0, 1.8, 0.0).normalise() * 10.25;
 
@@ -46,5 +46,49 @@ fn main() {
     }
 
     c.to_ppm(255, "test.ppm")
+}
 
+fn sphere_shadow_example() {
+
+    use intersection::{Object, hit};
+    use point::Point;
+    use color::Color;
+    use ray::Ray;
+
+    let canvas_pixels: usize = 100;
+    let mut c = canvas::Canvas::new(canvas_pixels, canvas_pixels);
+
+    let sphere_origin = Point::new(0.0, 0.0, 0.0);
+    let ray_origin = Point::new(0.0, 0.0, -5.0);
+
+    let wall_z: f32 = 10.0;
+    let wall_size: f32 = 7.0;
+
+    let pixel_size = wall_size / canvas_pixels as f32;
+
+    let half = wall_size / 2.0;
+
+    let shape = Object::new(sphere_origin);
+
+    for y in 0..canvas_pixels {
+        let world_y = half - pixel_size * y as f32;
+        for x in 0..canvas_pixels {
+            let world_x = -half + pixel_size * x as f32;
+            let position = Point::new(world_x, world_y, wall_z);
+
+            let r = Ray::new(ray_origin, (position - ray_origin).normalise());
+
+            match r.intersection(&shape) {
+                Ok(Some(result)) => c.write_pixel(x, y, Color::new(1.0, 0.0, 0.0)),
+                _ => (),
+            }
+        }
+    }
+    c.to_ppm(255, "sphere_shadow.ppm")
+
+}
+
+fn main() {
+    // projectile_example();
+    sphere_shadow_example();
 }
